4eb78f2a656619388457ddc62c2f9894efbb10cb
Amir Maleki <realmalekiamir@gmail.com>
2023-03-31 11:58:09 +0330

    make some changes

--- src/components/Profile.js
+++
@@ -1,8 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react";
-
-function Profile() {
-  const { user, isAuthenticated } = useAuth0();
-  return isAuthenticated && ;
-}
-
-export default Profile;
--- src/index.js
+++ src/index.js
@@ -4,22 +4,11 @@ import App from "./App";
 import { Provider } from "./context/weather";
 import "./index.css";
 
-import { Auth0Provider } from "@auth0/auth0-react";
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
   <Provider>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </Provider>
 );
